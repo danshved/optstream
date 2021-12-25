94f938d14d4c11d5d2c61b3e618612feef91d9bd
Dan Shved <danshved@gmail.com>
2021-12-25 22:25:01 +0100

    Test both helpful nad helpless atomic parsers

--- tests/OptStreamTest.hs
+++ tests/OptStreamTest.hs
@@ -14,34 +14,34 @@ main = defaultMain tests
 tests :: [Test]
 tests =
   [ testGroup "flag'"
-    [ testProperty "Matches"       prop_flag'_Matches
-    , testProperty "Finishes"      prop_flag'_Finishes
-    , testProperty "Empty"         prop_flag'_Empty
-    , testProperty "NotMatches"    prop_flag'_NotMatches
-    , testProperty "Skips"         prop_flag'_Skips
-    , testProperty "MatchesBundle" prop_flag'_MatchesBundle
+    [ testProperty "Matches"       prop_flag_Matches
+    , testProperty "Finishes"      prop_flag_Finishes
+    , testProperty "Empty"         prop_flag_Empty
+    , testProperty "NotMatches"    prop_flag_NotMatches
+    , testProperty "Skips"         prop_flag_Skips
+    , testProperty "MatchesBundle" prop_flag_MatchesBundle
     ]
 
   , testGroup "flagSep'"
-    [ testProperty "Matches"          prop_flagSep'_Matches
-    , testProperty "Finishes"         prop_flagSep'_Finishes
-    , testProperty "Empty"            prop_flagSep'_Empty
-    , testProperty "NotMatches"       prop_flagSep'_NotMatches
-    , testProperty "Skips"            prop_flagSep'_Skips
-    , testProperty "NotMatchesBundle" prop_flagSep'_NotMatchesBundle
+    [ testProperty "Matches"          prop_flagSep_Matches
+    , testProperty "Finishes"         prop_flagSep_Finishes
+    , testProperty "Empty"            prop_flagSep_Empty
+    , testProperty "NotMatches"       prop_flagSep_NotMatches
+    , testProperty "Skips"            prop_flagSep_Skips
+    , testProperty "NotMatchesBundle" prop_flagSep_NotMatchesBundle
     ]
 
   , testGroup "param'"
-    [ testProperty "Matches"       prop_param'_Matches
-    , testProperty "MatchesShort"  prop_param'_MatchesShort
-    , testProperty "MatchesLong"   prop_param'_MatchesLong
-    , testProperty "Finishes"      prop_param'_Finishes
-    , testProperty "FinishesShort" prop_param'_FinishesShort
-    , testProperty "FinishesLong"  prop_param'_FinishesLong
-    , testProperty "Empty"         prop_param'_Empty
-    , testProperty "MissingArg"    prop_param'_MissingArg
-    , testProperty "NotMatches"    prop_param'_NotMatches
-    , testProperty "Skips"         prop_param'_Skips
+    [ testProperty "Matches"       prop_param_Matches
+    , testProperty "MatchesShort"  prop_param_MatchesShort
+    , testProperty "MatchesLong"   prop_param_MatchesLong
+    , testProperty "Finishes"      prop_param_Finishes
+    , testProperty "FinishesShort" prop_param_FinishesShort
+    , testProperty "FinishesLong"  prop_param_FinishesLong
+    , testProperty "Empty"         prop_param_Empty
+    , testProperty "MissingArg"    prop_param_MissingArg
+    , testProperty "NotMatches"    prop_param_NotMatches
+    , testProperty "Skips"         prop_param_Skips
     ]
 
   , testGroup "freeArg'"
@@ -53,12 +53,12 @@ tests =
     ]
 
   , testGroup "multiParam'"
-    [ testProperty "Matches"    prop_multiParam'_Matches
-    , testProperty "Finishes"   prop_multiParam'_Finishes
-    , testProperty "Empty"      prop_multiParam'_Empty
-    , testProperty "NotEnough"  prop_multiParam'_NotEnough
-    , testProperty "NotMatches" prop_multiParam'_NotMatches
-    , testProperty "Skips"      prop_multiParam'_Skips
+    [ testProperty "Matches"    prop_multiParam_Matches
+    , testProperty "Finishes"   prop_multiParam_Finishes
+    , testProperty "Empty"      prop_multiParam_Empty
+    , testProperty "NotEnough"  prop_multiParam_NotEnough
+    , testProperty "NotMatches" prop_multiParam_NotMatches
+    , testProperty "Skips"      prop_multiParam_Skips
     ]
   ]
 
@@ -112,125 +112,164 @@ args :: Parser [String]
 args = many (anyArg' "ARG")
 
 
-prop_flag'_Matches (Legals as) (Legal b) (Legals cs) =
-  runParser (flag' forms) [b] == Right ()
+data FlagMaker
+  = Flag'
+  | Flag String
+  deriving Show
+
+mkFlag :: FlagMaker -> [OptionForm] -> Parser ()
+mkFlag Flag' opts = flag' opts
+mkFlag (Flag desc) opts = flag opts desc
+
+mkFlagSep :: FlagMaker -> [OptionForm] -> Parser ()
+mkFlagSep Flag' opts = flagSep' opts
+mkFlagSep (Flag desc) opts = flagSep opts desc
+
+instance Arbitrary FlagMaker where
+  arbitrary = oneof [pure Flag', Flag <$> arbitrary]
+
+  shrink Flag' = []
+  shrink (Flag s) = Flag':[Flag s' | s' <- shrink s]
+
+prop_flag_Matches maker (Legals as) (Legal b) (Legals cs) =
+  runParser (mkFlag maker forms) [b] == Right ()
   where
     forms = as ++ [b] ++ cs
 
-prop_flag'_Finishes (Legals as) (Legal b) (Legals cs) ds =
-  runParser (flag' forms *> args) (b:ds) == Right ds
+prop_flag_Finishes maker (Legals as) (Legal b) (Legals cs) ds =
+  runParser (mkFlag maker forms *> args) (b:ds) == Right ds
   where
     forms = as ++ [b] ++ cs
 
-prop_flag'_Skips (Legals as) (Legal b) (Legals cs) d =
+prop_flag_Skips maker (Legals as) (Legal b) (Legals cs) d =
   not (d `elem` forms) ==>
-  runParser (flag' forms #> args) [d, b] == Right [d]
+  runParser (mkFlag maker forms #> args) [d, b] == Right [d]
   where
     forms = as ++ [b] ++ cs
 
-prop_flag'_Empty (Legal a) (Legals bs) =
-  isLeft $ runParser (flag' forms) []
+prop_flag_Empty maker (Legal a) (Legals bs) =
+  isLeft $ runParser (mkFlag maker forms) []
   where
     forms = a:bs
 
-prop_flag'_NotMatches (Legal a) (Legals bs) c =
+prop_flag_NotMatches maker (Legal a) (Legals bs) c =
   not (c `elem` forms) ==>
-  isLeft $ runParser (flag' forms) [c]
+  isLeft $ runParser (mkFlag maker forms) [c]
   where
     forms = a:bs
 
-prop_flag'_MatchesBundle cs =
-  length cs /= 0 ==>
-  runParser p ['-':map unNotDash cs] == Right [() | c <- cs]
+prop_flag_MatchesBundle maker (NonEmpty cs) =
+  runParser p ['-':chars] == Right [() | c <- chars]
   where
-    p = sequenceA [flag' [['-', c]] | NotDash c <- cs]
+    p = sequenceA [mkFlag maker [['-', c]] | c <- chars]
+    chars = [c | NotDash c <- cs]
 
 
-prop_flagSep'_Matches (Legals as) (Legal b) (Legals cs) =
-  runParser (flagSep' forms) [b] == Right ()
-  where
-    forms = as ++ [b] ++ cs
-
-prop_flagSep'_Finishes (Legals as) (Legal b) (Legals cs) ds =
-  runParser (flagSep' forms *> args) (b:ds) == Right ds
+prop_flagSep_Matches maker (Legals as) (Legal b) (Legals cs) =
+  runParser (mkFlagSep maker forms) [b] == Right ()
   where
     forms = as ++ [b] ++ cs
 
-prop_flagSep'_Skips (Legals as) (Legal b) (Legals cs) d =
+prop_flagSep_Finishes maker (Legals as) (Legal b) (Legals cs) ds =
+  runParser (mkFlagSep maker forms *> args) (b:ds) == Right ds
+  where
+    forms = as ++ [b] ++ cs
+
+prop_flagSep_Skips maker (Legals as) (Legal b) (Legals cs) d =
   not (d `elem` forms) ==>
-  runParser (flagSep' forms #> args) [d, b] == Right [d]
+  runParser (mkFlagSep maker forms #> args) [d, b] == Right [d]
   where
     forms = as ++ [b] ++ cs
 
-prop_flagSep'_Empty (Legal a) (Legals bs) =
-  isLeft $ runParser (flagSep' forms) []
+prop_flagSep_Empty maker (Legal a) (Legals bs) =
+  isLeft $ runParser (mkFlagSep maker forms) []
   where
     forms = a:bs
 
-prop_flagSep'_NotMatches (Legal a) (Legals bs) c =
+prop_flagSep_NotMatches maker (Legal a) (Legals bs) c =
   not (c `elem` forms) ==>
-  isLeft $ runParser (flagSep' forms) [c]
+  isLeft $ runParser (mkFlagSep maker forms) [c]
   where
     forms = a:bs
 
-prop_flagSep'_NotMatchesBundle cs =
+prop_flagSep_NotMatchesBundle maker cs =
   length cs >= 2 ==>
   isLeft $ runParser p ['-':map unNotDash cs]
   where
-    p = sequenceA [flagSep' [['-', c]] | NotDash c <- cs]
+    p = sequenceA [mkFlagSep maker [['-', c]] | NotDash c <- cs]
 
 
-prop_param'_Matches (Legals as) (Legal b) (Legals cs) meta d =
-  runParser (param' forms meta) [b, d] == Right d
+data ParamMaker
+  = Param' String
+  | Param String String
+  deriving Show
+
+mkParam :: ParamMaker -> [OptionForm] -> Parser String
+mkParam (Param' metavar) opts = param' opts metavar
+mkParam (Param metavar desc) opts = param opts metavar desc
+
+instance Arbitrary ParamMaker where
+  arbitrary = oneof
+    [ Param' <$> arbitrary
+    , Param <$> arbitrary <*> arbitrary
+    ]
+
+  shrink (Param' metavar) = [Param' metavar' | metavar' <- shrink metavar]
+  shrink (Param metavar desc) = [Param' metavar] ++
+    [Param metavar desc' | desc' <- shrink desc] ++
+    [Param metavar' desc | metavar' <- shrink metavar]
+
+prop_param_Matches maker (Legals as) (Legal b) (Legals cs) d =
+  runParser (mkParam maker forms) [b, d] == Right d
   where
     forms = as ++ [b] ++ cs
 
-prop_param'_MatchesShort (Legals as) (NotDash b) (Legals cs) meta (NonEmpty d) =
-  runParser (param' forms meta) ['-':b:d] == Right d
+prop_param_MatchesShort maker (Legals as) (NotDash b) (Legals cs) (NonEmpty d) =
+  runParser (mkParam maker forms) ['-':b:d] == Right d
   where
     forms = as ++ [['-', b]] ++ cs
 
-prop_param'_MatchesLong (Legals as) (NonEmpty b) (Legals cs) meta d =
-  runParser (param' forms meta) ["--" ++ b ++ "=" ++ d] == Right d
+prop_param_MatchesLong maker (Legals as) (NonEmpty b) (Legals cs) d =
+  runParser (mkParam maker forms) ["--" ++ b ++ "=" ++ d] == Right d
   where
     forms = as ++ ["--" ++ b] ++ cs
 
-prop_param'_Finishes (Legals as) (Legal b) (Legals cs) meta d es =
-  runParser (param' forms meta *> args) (b:d:es) == Right es
+prop_param_Finishes maker (Legals as) (Legal b) (Legals cs) d es =
+  runParser (mkParam maker forms *> args) (b:d:es) == Right es
   where
     forms = as ++ [b] ++ cs
 
-prop_param'_FinishesShort
-  (Legals as) (NotDash b) (Legals cs) meta (NonEmpty d) es =
-  runParser (param' forms meta *> args) (('-':b:d):es) == Right es
+prop_param_FinishesShort
+  maker (Legals as) (NotDash b) (Legals cs) (NonEmpty d) es =
+  runParser (mkParam maker forms *> args) (('-':b:d):es) == Right es
   where
     forms = as ++ [['-', b]] ++ cs
 
-prop_param'_FinishesLong (Legals as) (NonEmpty b) (Legals cs) meta d es =
-  runParser (param' forms meta *> args) (assign:es) == Right es
+prop_param_FinishesLong maker (Legals as) (NonEmpty b) (Legals cs) d es =
+  runParser (mkParam maker forms *> args) (assign:es) == Right es
   where
     forms = as ++ ["--" ++ b] ++ cs
     assign = "--" ++ b ++ "=" ++ d
 
-prop_param'_Skips (Legals as) (Legal b) (Legals cs) meta d e =
+prop_param_Skips maker (Legals as) (Legal b) (Legals cs) d e =
   not (any (`isPrefixOf` e) forms) ==>
-  runParser (param' forms meta #> args) [e, b, d] == Right [e]
+  runParser (mkParam maker forms #> args) [e, b, d] == Right [e]
   where
     forms = as ++ [b] ++ cs
 
-prop_param'_Empty (Legal a) (Legals bs) meta =
-  isLeft $ runParser (param' forms meta) []
+prop_param_Empty maker (Legal a) (Legals bs) =
+  isLeft $ runParser (mkParam maker forms) []
   where
     forms = a:bs
 
-prop_param'_MissingArg (Legals as) (Legal b) (Legals cs) meta =
-  isLeft $ runParser (param' forms meta) [b]
+prop_param_MissingArg maker (Legals as) (Legal b) (Legals cs) =
+  isLeft $ runParser (mkParam maker forms) [b]
   where
     forms = as ++ [b] ++ cs
 
-prop_param'_NotMatches (Legal a) (Legals bs) meta c d =
+prop_param_NotMatches maker (Legal a) (Legals bs) c d =
   not (any (`isPrefixOf` c) forms) ==>
-  isLeft $ runParser (param' forms meta *> args) [c, d]
+  isLeft $ runParser (mkParam maker forms *> args) [c, d]
   where
     forms = a:bs
 
@@ -238,11 +277,11 @@ prop_param'_NotMatches (Legal a) (Legals bs) meta c d =
 data FreeArgMaker
   = FreeArg' String
   | FreeArg String String
-  deriving (Eq, Ord, Show)
+  deriving Show
 
-toFreeArg :: FreeArgMaker -> Parser String
-toFreeArg (FreeArg' meta) = freeArg' meta
-toFreeArg (FreeArg meta desc) = freeArg meta desc
+mkFreeArg :: FreeArgMaker -> Parser String
+mkFreeArg (FreeArg' meta) = freeArg' meta
+mkFreeArg (FreeArg meta desc) = freeArg meta desc
 
 instance Arbitrary FreeArgMaker where
   arbitrary = oneof
@@ -256,62 +295,79 @@ instance Arbitrary FreeArgMaker where
     [FreeArg meta' desc | meta' <- shrink meta]
 
 prop_freeArg_Matches maker (Free a) =
-  runParser (toFreeArg maker) [a] == Right a
+  runParser (mkFreeArg maker) [a] == Right a
 
 prop_freeArg_Finishes maker (Free a) bs =
-  runParser (toFreeArg maker *> args) (a:bs) == Right bs
+  runParser (mkFreeArg maker *> args) (a:bs) == Right bs
 
 prop_freeArg_Skips maker (Free a) b =
-  runParser (toFreeArg maker #> args) ['-':b, a] == Right ['-':b]
+  runParser (mkFreeArg maker #> args) ['-':b, a] == Right ['-':b]
 
 prop_freeArg_Empty maker =
-  isLeft $ runParser (toFreeArg maker) []
+  isLeft $ runParser (mkFreeArg maker) []
 
 prop_freeArg_NotMatches maker a =
-  isLeft $ runParser (toFreeArg maker) ['-':a]
+  isLeft $ runParser (mkFreeArg maker) ['-':a]
 
 
-prop_multiParam'_Matches (Legals as) (Legal b) (Legals cs) dms =
-  runParser (multiParam' forms $ traverse next ms) (b:ds) == Right ds
+data MultiParamMaker
+  = MultiParam'
+  | MultiParam String
+  deriving Show
+
+mkMultiParam :: MultiParamMaker -> [OptionForm] -> Follower a -> Parser a
+mkMultiParam MultiParam' opts f = multiParam' opts f
+mkMultiParam (MultiParam desc) opts f = multiParam opts f desc
+
+instance Arbitrary MultiParamMaker where
+  arbitrary = oneof [pure MultiParam', MultiParam <$> arbitrary]
+
+  shrink MultiParam' = []
+  shrink (MultiParam desc) = MultiParam':
+    [MultiParam desc' | desc' <- shrink desc]
+
+prop_multiParam_Matches maker (Legals as) (Legal b) (Legals cs) dms =
+  runParser (mkMultiParam maker forms $ traverse next ms) (b:ds) == Right ds
   where
     forms = as ++ [b] ++ cs
     ms = map snd dms  -- Metavariables.
     ds = map fst dms  -- Arguments that should match them.
 
-prop_multiParam'_Finishes (Legals as) (Legal b) (Legals cs) dms es =
-  runParser (multiParam' forms f *> args) ([b] ++ ds ++ es) == Right es
+prop_multiParam_Finishes maker (Legals as) (Legal b) (Legals cs) dms es =
+  runParser (mkMultiParam maker forms f *> args) ([b] ++ ds ++ es) == Right es
   where
     forms = as ++ [b] ++ cs
     f = traverse next ms
     ms = map snd dms  -- Metavariables.
     ds = map fst dms  -- Arguments that should match them.
 
-prop_multiParam'_Skips (Legals as) (Legal b) (Legals cs) dms e =
+prop_multiParam_Skips maker (Legals as) (Legal b) (Legals cs) dms e =
   not (e `elem` forms) ==>
-  runParser (multiParam' forms f #> args) (e:b:ds) == Right [e]
+  runParser (mkMultiParam maker forms f #> args) (e:b:ds) == Right [e]
   where
     forms = as ++ [b] ++ cs
     f = traverse next ms
     ms = map snd dms  -- Metavariables.
     ds = map fst dms  -- Arguments that should match them.
 
-prop_multiParam'_Empty (Legal a) (Legals bs) ms =
-  isLeft $ runParser (multiParam' forms f) []
+prop_multiParam_Empty maker (Legal a) (Legals bs) ms =
+  isLeft $ runParser (mkMultiParam maker forms f) []
   where
     forms = a:bs
     f :: Follower [String]
     f = traverse next ms
 
-prop_multiParam'_NotEnough (Legals as) (Legal b) (Legals cs) dms (NonEmpty ms) =
-  isLeft $ runParser (multiParam' forms $ traverse next ms') (b:ds)
+prop_multiParam_NotEnough
+  maker (Legals as) (Legal b) (Legals cs) dms (NonEmpty ms) =
+  isLeft $ runParser (mkMultiParam maker forms $ traverse next ms') (b:ds)
   where
     forms = as ++ [b] ++ cs
     ms' = map snd dms ++ ms  -- Metavariables.
     ds = map fst dms         -- Arguments that should match part of them.
 
-prop_multiParam'_NotMatches (Legal a) (Legals bs) ms c cs =
+prop_multiParam_NotMatches maker (Legal a) (Legals bs) ms c cs =
   not (c `elem` forms) ==>
-  isLeft $ runParser (multiParam' forms f *> args) (c:cs)
+  isLeft $ runParser (mkMultiParam maker forms f *> args) (c:cs)
   where
     forms = a:bs
     f :: Follower [String]
