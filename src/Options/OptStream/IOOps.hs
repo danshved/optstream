700678e05f7f681ffaaecda0e5418405445b7430
Dan Shved <danshved@gmail.com>
2022-01-11 19:53:37 +0100

    Add haddock to IOOps

--- src/Options/OptStream/IOOps.hs
+++ src/Options/OptStream/IOOps.hs
@@ -1,9 +1,22 @@
+{-|
+Module      : Options.OptStream.Raw
+Copyright   : (c) Dan Shved, 2022
+License     : BSD-3
+Maintainer  : danshved@gmail.com
+Stability   : experimental
+
+This module defines 'IOOps', which is a way to abstract away IO operations
+required for "Options.OptStream".
+-}
 module Options.OptStream.IOOps where
 
 import qualified System.Exit
 import qualified System.Environment
 import qualified System.IO
 
+-- | 'IOOps' is a typeclass that describes parts of 'IO' used by
+-- "Options.OptStream". It is meant to be represented by 'IO' in production and
+-- a mock implementation in tests.
 class Monad m => IOOps m where
   getArgs :: m [String]
   getProgName :: m String
@@ -18,4 +31,3 @@ instance IOOps IO where
   die = System.Exit.die
   exitSuccess = System.Exit.exitSuccess
 
--- TODO: Add haddock.
